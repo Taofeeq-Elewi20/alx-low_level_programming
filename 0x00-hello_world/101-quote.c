7db16fa4a7c5bdb8bc7cf6669c404caedd587569
Taofeeq-Elewi20 <tatanda118@gmail.com>
2023-08-11 01:42:04 -0700

    Hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include <stdio.h>
 
 /**
@@ -7,7 +8,6 @@
  */
 int main(void)
 {
-	fwrite(2,
-	"and that piece of art is useful\" - Dora korpar,2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
 	return (1);
 }
