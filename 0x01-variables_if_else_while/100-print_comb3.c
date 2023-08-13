e7a4df7a529443e6de3fb98e025ee8b7cd0a522d
Taofeeq-Elewi20 <tatanda118@gmail.com>
2023-08-13 08:21:42 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,18 +12,21 @@ int main(void)
 
 	for (i = 0 ; i <= 10 ; i++)
 	{
-		for (j = i + '1' ; j < 10 ; j++)
+		for (j = i + '1' ; j <= 10 ; j++)
 		{
-			putchar(i + '0');
-			putchar(j + '0');
-
-			if (i != 8 || j != 9)
+			if (j != i)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(i);
+				putchar(j);
+				{
+					if (i == '8' && j == '9')
+						continue;
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
+	putchar('\n');
 	return (0);
 }
-
