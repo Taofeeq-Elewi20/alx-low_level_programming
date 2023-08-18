2d28f8d0bab7c8b1e15ea820c864a7e6a9e37928
Taofeeq-Elewi20 <tatanda118@gmail.com>
2023-08-18 03:24:11 -0700

    Task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,17 +8,17 @@
 
 void print_diagonal(int n)
 {
+	int i, j;
+	
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
-		int i, j;
-
 		for (i = 0 ; i < n ; i++)
 		{
 			for (j = 0 ; j <= i ; j++)
-				_putchar(' ');
-			_putchar('\\');
+				_putchar(32);
+			_putchar(92);
 			_putchar('\n');
 		}
 	}
