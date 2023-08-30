4a1b87739aaf8c06fc6a1dd7c33b20b3c32cf08b
Taofeeq-Elewi20 <tatanda118@gmail.com>
2023-08-30 08:55:32 -0700

    Task 9

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - Entry point
+ * @s: input parameter 1
+ * @to: input parameter 2
+ * Return: Always 0.
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
